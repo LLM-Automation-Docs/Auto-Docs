602a1c1d43bfea64b83d7ed2d0410bf7bb5dc612
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2025-04-21 18:36:43 +0200

    Test2 part 2

--- main/controllers/car/CarRentalService.kt
+++ main/controllers/car/CarRentalService.kt
@@ -3,4 +3,5 @@ package main.controllers.car
 interface CarRentalService {
     suspend fun isOpenNow(): Boolean
     suspend fun getAvailableCars(): List<Car>
+    suspend fun getCar(id: Int): Car?
 }
